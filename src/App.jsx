5eaf4dba0565253a66cc1ecc268d9899fb9f173f
paul-tes <paulasca8462@gmail.com>
2024-10-15 15:58:18 +0300

    Add Song Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import SearchBar from './components/SearchBar'
-
+import Song from './components/Song'
 function App() {
   return (
     <div className='flex flex-col gap-2'>
       <NavBar />
       <SearchBar />
+      <div className='carousel carousel-center rounded-box  space-x-4 p-4'>
+        <Song  className='carousel-item' />
+        <Song className='carousel-item' />
+        <Song className='carousel-item' />
+        <Song className='carousel-item' />
+        <Song className='carousel-item' />
+        
+      </div>
     </div>
   )
 }
