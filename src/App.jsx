a4b920bd8d3b310097b27b22f7b36244753c8de2
paul-tes <paulasca8462@gmail.com>
2024-10-15 14:24:05 +0300

    Add Search bar for searching song

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import SearchBar from './components/SearchBar'
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col gap-2'>
       <NavBar />
-    </>
+      <SearchBar />
+    </div>
   )
 }
 
